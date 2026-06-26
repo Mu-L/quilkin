292c829ba932b01c947b37dac08198320be8266b
Jake Shadle <jake.shadle@embark-studios.com>
2026-06-26 10:05:09 +0200

    Update the list of kernel versions we test against (#1421)

--- .ci/xdp/integ-test-multi-kernel.sh
+++ .ci/xdp/integ-test-multi-kernel.sh
@@ -2,8 +2,12 @@
 set -e
 
 # We _cannot_ test on 6.17 due to a kernel bug
-KERNEL_VERSIONS=('6.11' '6.12' '6.13' '6.14' '6.16') #'6.15'
- 
+# - 6.12 is the current version supported on Debian Trixie
+# - 6.18 is the latest LTS release
+# - 6.19 is the latest 6.x release (kind of)
+# - 7.0 is the latest, but isn't available on the ubuntu repo as of yet
+KERNEL_VERSIONS=('6.12' '6.13' '6.14' '6.16' '6.18' '6.19')
+
 for vers in "${KERNEL_VERSIONS[@]}"
 do
   echo "::group::Testing in kernel $vers"
